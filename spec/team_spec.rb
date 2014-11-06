ba77b03767d1984245cd7b4fffc701633a14464f
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-06 17:07:41 -0500

    added team describe

---
+++ spec/team_spec.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/team.rb"
+
+describe Team do
+	it "has a name"
+	it "has a list of players"
+	it "is favored if it has a celebrity in it"
+	it "complains if there is a bad word in the name"
+end
