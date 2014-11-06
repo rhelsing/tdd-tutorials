ac0b290512003c13b48904e50cc034eee8f7930c
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-06 17:32:33 -0500

    updates syntax

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,16 +2,16 @@ require_relative "../lib/team.rb"
 
 describe Team do
 	it "has a name" do
-		Team.new("hey").should respond_to :name
+		expect(Team.new("hey")).to respond_to :name
 	end
 	it "has a list of players" do
-		Team.new("hey").players.should be_kind_of Array
+		expect(Team.new("hey").players).to be_kind_of Array
 	end
 	it "is favored if it has a celebrity in it" do
-		Team.new("hey", ["George Clooney", "Ryan"]).should be_favored
+		expect(Team.new("hey", ["George Clooney", "Ryan"])).to be_favored
 	end
 	it "complains if there is a bad word in the name" do
-		expect {Team.new("Crappy name") }.to raise_error
+		expect {Team.new("Crappy name") }.to raise_error #need brackets for some reason?
 	end
 
 	context "given a bad list of players" do
